d26f1b1d00f27fd4b9e5ee74bd77a7573cdc780f
aUserSM <163421168+aUserSM@users.noreply.github.com>
2024-04-06 11:16:28 +0300

    Update main.c

--- Real/main.c
+++ Real/main.c
@@ -1,4 +1,4 @@
-#include"functions.c"
+#include"visual.c"
 
 /*
 SDL credits in SDL_IMPORTANT_NOTES > CREDITS
@@ -12,8 +12,8 @@ gcc -Isrc/Include -Lsrc/lib -o run main.c -lmingw32 -lSDL2main -lSDL2
 int main(int argc, char* args[])
 {
     int running = 1;
-    int window_height = 640;
-    int window_with = 480;
+    int window_height = 480;
+    int window_with = 640;
 
     // Set up SDL.
     SDL_Window *window = NULL;
@@ -27,6 +27,15 @@ int main(int argc, char* args[])
     SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND); // Enables alpha blending.
 
     // SDL_SetWindowResizable(window, SDL_TRUE);
+
+    // Texture
+    surface = SDL_LoadBMP("textures\\test_map.bmp");
+    SDL_Texture* texture_map = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_Rect rect;
+    rect.w = window_with;
+    rect.h = window_height;
+    rect.y = 0;
+    rect.x = 0;
 
     // Main loop.
     while(running){
@@ -50,13 +59,29 @@ int main(int argc, char* args[])
         // Logic.
 
         // Rendering.
-        
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        // Give rect a color.
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+        // Draw.
+        SDL_RenderDrawRect(renderer, &rect);
+
+        SDL_RenderCopy(renderer, texture_map, NULL, &rect); // 1st NULL can be replaced to say what part to copy, NULL says that copy all.
+
+        SDL_RenderPresent(renderer); // Update window.
     }
 
     // Start exiting.
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
+
+    // Free textures:
+    SDL_FreeSurface(surface);
+
+    SDL_DestroyTexture(texture_map);
 
     return 0;
 }
